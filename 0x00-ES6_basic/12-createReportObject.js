637e5e9b5ed9d60017b900625e292134983dcdb6
kiplimoboor <kiplimoarapboor@gmail.com>
2024-04-22 22:27:01 +0300

    length

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      employeesList,
+    },
+    getNumberOfDepartments(depts) {
+      return Object.keys(depts.employeesList).length;
+    },
+  };
+}
