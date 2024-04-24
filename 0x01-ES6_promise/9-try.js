939a412e97cbf15bfdad8ca949fb8e29ba8755f1
kiplimoboor <kiplimoarapboor@gmail.com>
2024-04-24 20:29:25 +0300

    try catch finally

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+  } catch (e) {
+    queue.push(e.message);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
