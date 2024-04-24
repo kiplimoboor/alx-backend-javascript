ec3e4deed4ec0fab2ca87251bd74e6666dce54b8
kiplimoboor <kiplimoarapboor@gmail.com>
2024-04-24 11:58:33 +0300

    then

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then(console.log('Got a response from the API')).catch(new Error());
+  return {
+    status: 200,
+    body: 'success',
+  };
+}
