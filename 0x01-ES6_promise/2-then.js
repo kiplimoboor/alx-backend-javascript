5bee8e2b041cc5e2c239cd259dec1805719f3ae0
kiplimoboor <kiplimoarapboor@gmail.com>
2024-04-24 12:23:15 +0300

    then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(console.log('Got a response from the API')).catch(new Error());
-  return {
-    status: 200,
-    body: 'success',
-  };
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
