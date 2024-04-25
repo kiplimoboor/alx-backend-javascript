50144eacff809f5ee0c0ca1f073209e1e57c272f
kiplimoboor <kiplimoarapboor@gmail.com>
2024-04-26 00:04:16 +0300

    abstract class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,23 +2,21 @@
 
 export default class Building {
   constructor(sqft) {
-    if (
-      this.constructor !== Building &&
-      this.evacuationWarningMessage === undefined
-    ) {
-      throw new Error(
-        'Class extending Building must override evacuationWarningMessage'
-      );
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage'
+        );
+      }
     }
-
-    if (typeof sqft !== 'number') {
-      throw new Error('sqft must be a number');
-    }
-    this._sqft = sqft;
   }
 
-  // Get sqft
   get sqft() {
-    return this.sqft;
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
   }
 }
