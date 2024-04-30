48de0ec30da69cb971071d55126cf414e907cfb8
kiplimoboor <kiplimoarapboor@gmail.com>
2024-04-30 13:28:34 +0300

    set iterate

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,11 @@
+/* eslint-disable operator-linebreak */
 export default function cleanSet(set, startString) {
-  if (!startString) {
+  if (
+    !set ||
+    !startString ||
+    !(set instanceof Set) ||
+    typeof startString !== 'string'
+  ) {
     return '';
   }
 
