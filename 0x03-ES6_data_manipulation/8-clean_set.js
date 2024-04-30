f7dd8bbee3930396054f2f2f6e08dde475d1cb9d
kiplimoboor <kiplimoarapboor@gmail.com>
2024-04-30 14:23:20 +0300

    set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,24 @@
 /* eslint-disable operator-linebreak */
 export default function cleanSet(set, startString) {
   if (
-    !(set instanceof Set) ||
-    set.size === 0 ||
-    typeof startString !== 'string' ||
-    startString.length === 0
+    !set &&
+    !startString &&
+    !(set instanceof Set) &&
+    typeof startString !== 'string'
   ) {
     return '';
   }
-  return [...set]
-    .filter((word) => word.includes(startString))
-    .map((word) => word.substring(startString.length))
-    .join('-');
+
+  const parts = [];
+
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
