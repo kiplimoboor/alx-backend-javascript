09f441724223dc4d2ae9b2267d4c033dcab4c6bc
kiplimoboor <kiplimoarapboor@gmail.com>
2024-04-30 14:21:15 +0300

    set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,12 +2,13 @@
 export default function cleanSet(set, startString) {
   if (
     !(set instanceof Set) ||
+    set.size === 0 ||
     typeof startString !== 'string' ||
     startString.length === 0
   ) {
     return '';
   }
-  return Array.from(set)
+  return [...set]
     .filter((word) => word.includes(startString))
     .map((word) => word.substring(startString.length))
     .join('-');
