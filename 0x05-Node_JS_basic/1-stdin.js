16a640593d9136ea99435068d4cefbfd82afbe46
kiplimoboor <kiplimoarapboor@gmail.com>
2024-06-17 20:19:36 +0300

    read cli input

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,16 @@
-const readline = require('node:readline');
-
-const rl = readline.createInterface({
+const readline = require('node:readline').createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  rl.close();
+readline.question(
+  'Welcome to Holberton School, what is your name? \n',
+  (name) => {
+    console.log(`Your name is: ${name}`);
+    readline.close();
+  }
+);
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing\n');
 });
