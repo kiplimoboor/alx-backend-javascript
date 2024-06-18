d6db6b00d0d87efdce554c7de993fd74b104bc3f
kiplimoboor <kiplimoarapboor@gmail.com>
2024-06-18 17:11:53 +0300

    read file synchronous

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,35 +2,8 @@ const fs = require('node:fs');
 
 function countStudents(path) {
   try {
-    const students = fs
-      .readFileSync(path)
-      .toString('utf-8')
-      .trim()
-      .split('\n')
-      .slice(1);
-
-    const groupedStudents = {};
-
-    for (let student of students) {
-      student = student.split(',');
-      const firstName = student[0];
-      const course = student[3];
-
-      if (!groupedStudents.hasOwnProperty(course)) {
-        groupedStudents[course] = [firstName];
-      } else {
-        groupedStudents[course].push(firstName);
-      }
-    }
-
-    console.log('Number of students: ' + students.length);
-
-    for (const group in groupedStudents) {
-      const studentsInGroup = groupedStudents[group];
-      console.log(
-        `Number of students in ${group}: ${studentsInGroup.length}. List: ${studentsInGroup.join(', ')}`
-      );
-    }
+    const students = fs.readFileSync(path).toString('utf-8').trim().split('\n');
+    console.log(students);
   } catch (e) {
     throw new Error('Cannot load the database');
   }
