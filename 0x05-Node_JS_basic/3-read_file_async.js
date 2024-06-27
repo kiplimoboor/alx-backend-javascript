0a724d2543a4fc442c7ee2ba2cf0131881311dde
kiplimoboor <kiplimoarapboor@gmail.com>
2024-06-27 21:54:31 +0300

    read async

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,42 +1,33 @@
-const { readFile } = require('fs');
+/* eslint-disable */
 
-function countStudents(fileName) {
-  const students = {};
-  const fields = {};
-  let length = 0;
+const fs = require('fs');
+
+function countStudents(path) {
   return new Promise((resolve, reject) => {
-    readFile(fileName, (error, data) => {
-      if (error) {
-        reject(Error('Cannot load the database'));
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
       }
-      const lines = data.toString().split('\n');
-      for (let i = 0; i < lines.length; i += 1) {
-        if (lines[i]) {
-          length += 1;
-          const field = lines[i].toString().split(',');
-          if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-            students[field[3]].push(field[0]);
-          } else {
-            students[field[3]] = [field[0]];
-          }
-          if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-            fields[field[3]] += 1;
-          } else {
-            fields[field[3]] = 1;
-          }
+      const students = data.toString().trim().split('\n').slice(1);
+      const studentsByCourse = {};
+      students.map((student) => {
+        const studentInfo = student.split(',');
+        const course = studentInfo[3];
+        const firstName = studentInfo[0];
+        if (Object.prototype.hasOwnProperty.call(studentsByCourse, course)) {
+          studentsByCourse[course].push(firstName);
+        } else {
+          studentsByCourse[course] = [firstName];
         }
-
-        const l = length - 1;
-        console.log(`Number of students: ${l}`);
-        for (const [key, value] of Object.entries(fields)) {
-          if (key !== 'field') {
-            console.log(
-              `Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`
-            );
-          }
-        }
-        resolve(data);
+      });
+      console.log(`Number of students: ${students.length}`);
+      for (const course in studentsByCourse) {
+        const students = studentsByCourse[course];
+        console.log(
+          `Number of students in ${course}: ${students.length}. List: ${students.join(', ')}`
+        );
       }
+      resolve(data);
     });
   });
 }
