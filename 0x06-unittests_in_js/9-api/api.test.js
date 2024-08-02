234a404810d823ef4f97fa442bc6a3e0c7d39a8a
kiplimoboor <kiplimoarapboor@gmail.com>
2024-08-02 15:33:44 +0300

    more tests

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,16 +2,36 @@ const { expect } = require('chai');
 const { response } = require('express');
 const request = require('request');
 
-describe('Cart Page', () => {
-  it('solves if path param is 10 (int)', () => {
-    request('http://localhost:7865/cart/10', (err, res, body) => {
+describe('Index page', () => {
+  it('correct status code', () => {
+    request('http://localhost:7865', (err, res, body) => {
       expect(res.statusCode).to.equal(200);
     });
   });
 
-  it('solves if path param is 10 (int)', () => {
-    request('http://localhost:7865/cart/wrs', (err, res, body) => {
+  it('correct body', () => {
+    request('http://localhost:7865', (err, res, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+    });
+  });
+});
+
+describe('Cart page', () => {
+  it('correct 200 code', () => {
+    request('http://localhost:7865/cart/1', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+    });
+  });
+
+  it('correct 404 code', () => {
+    request('http://localhost:7865/cart/str', (err, res, body) => {
       expect(res.statusCode).to.equal(404);
+    });
+  });
+
+  it('correct message', () => {
+    request('http://localhost:7865/cart/1', (err, res, body) => {
+      expect(body).to.deep.equal('Payment methods for cart 1');
     });
   });
 });
