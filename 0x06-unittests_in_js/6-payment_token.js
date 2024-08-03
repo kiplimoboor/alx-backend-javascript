7d9be2cd369b9e5ab653f14db70fae8e7d9993c3
kiplimoboor <kiplimoarapboor@gmail.com>
2024-08-03 12:05:44 +0300

    rm future files

--- 0x06-unittests_in_js/6-payment_token.js
+++
@@ -1,9 +0,0 @@
-function getPaymentTokenFromAPI(success) {
-  if (success) {
-    return new Promise((resolve, _) => {
-      resolve({ data: 'Successful response from the API' });
-    });
-  }
-}
-
-module.exports = getPaymentTokenFromAPI;
