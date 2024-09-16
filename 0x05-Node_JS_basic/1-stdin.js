a878cc35b73f04a8490ee85aa99e1a1a59049c3d
kiplimoboor <kiplimoarapboor@gmail.com>
2024-09-16 20:47:08 +0300

    stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', () => {
+  let chunk;
+  while ((chunk = process.stdin.read()) !== null) {
+    process.stdout.write('Your name is: ' + chunk);
+  }
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
