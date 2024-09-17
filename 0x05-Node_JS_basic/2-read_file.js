fa9011dc284eff99ecb4b7849799c206b93f104c
kiplimoboor <kiplimoarapboor@gmail.com>
2024-09-17 14:18:56 +0300

    read file sync

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,32 @@
+/* eslint-disable array-callback-return */
+
+const fs = require('fs');
+function countStudents (filepath) {
+  try {
+    const data = fs.readFileSync(filepath).toString().split('\n').slice(1, -1);
+    console.log('Number of students: ' + data.length);
+
+    const studentsByField = {};
+    data.map((student) => {
+      const studentInfo = student.split(',');
+      const firstName = studentInfo[0];
+      const field = studentInfo.slice(-1);
+      if (field in studentsByField) {
+        studentsByField[field].push(firstName);
+      } else {
+        studentsByField[field] = [firstName];
+      }
+    });
+
+    for (const field in studentsByField) {
+      const students = studentsByField[field];
+      console.log(
+        `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`
+      );
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
