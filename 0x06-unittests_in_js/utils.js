4f56382d6af2ecede697764926518d59d3120be2
kiplimoboor <kiplimoarapboor@gmail.com>
2024-09-20 14:55:24 +0300

    spy

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,16 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+    const [first, second] = [Math.round(a), Math.round(b)];
+    switch (type) {
+      case 'SUM':
+        return first + second;
+      case 'SUBTRACT':
+        return first - second;
+      case 'DIVIDE':
+        if (second == 0) return 'Error';
+        return first / second;
+    }
+  },
+};
+
+module.exports = Utils;
