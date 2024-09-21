57cfd240f9d04bf433fb3dd589565db629aff6e1
kiplimoboor <kiplimoarapboor@gmail.com>
2024-09-21 21:31:45 +0300

    add encoder

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -15,10 +15,9 @@ app.get('/available_payments', (req, res) => {
   res.send({ payment_methods: { credit_cards: true, paypal: false } });
 });
 
+app.use(express.json());
 app.post('/login', (req, res) => {
-  let username = '';
-  if (req.body) username = req.body.userName;
-  res.send(`Welcome ${username}`);
+  if (req.body) res.send(`Welcome ${req.body.userName}`);
 });
 
 app.listen(port, () => console.log('API available on localhost port ' + port));
