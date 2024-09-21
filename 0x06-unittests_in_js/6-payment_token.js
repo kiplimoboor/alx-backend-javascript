61ef8765f0c30884148e9fb129b924e16a887cfe
kiplimoboor <kiplimoarapboor@gmail.com>
2024-09-21 14:49:02 +0300

    async

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,6 +1,8 @@
-function getPaymentTokenFromAPI(sucess) {
-  if (sucess)
-    return Promise.resolve({ data: 'Successful response from the API' });
-}
+const getPaymentTokenFromAPI = (success) =>
+  new Promise((resolve, _reject) => {
+    if (success) {
+      resolve({ data: 'Successful response from the API' });
+    }
+  });
 
 module.exports = getPaymentTokenFromAPI;
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,10 +2,10 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('should resolve if arg is true', (done) => {
+  it('getPaymentTokenFromAPI(success), where success == true', (done) => {
     getPaymentTokenFromAPI(true).then((res) => {
       expect(res).to.deep.equal({ data: 'Successful response from the API' });
+      done();
     });
-    done();
   });
 });
