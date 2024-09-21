b69f7ec5dd5433171901716427f37749d039f9a7
kiplimoboor <kiplimoarapboor@gmail.com>
2024-09-21 14:43:32 +0300

    async

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -4,7 +4,7 @@ const getPaymentTokenFromAPI = require('./6-payment_token');
 describe('getPaymentTokenFromAPI', () => {
   it('should resolve if arg is true', (done) => {
     getPaymentTokenFromAPI(true).then((result) => {
-      expect('data' in result).to.be.true;
+      expect(typeof result == 'object').to.be.true;
     });
     done();
   });
