dc7b0d25d155d5a44ebc38afba8cdca43b925cbb
kiplimoboor <kiplimoarapboor@gmail.com>
2024-09-21 13:34:01 +0300

    spy console

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -4,12 +4,30 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('uses the calculateNumber method of utils', () => {
-    const spy = sinon.spy(Utils);
+  let consoleSpy;
+  let calculateSpy;
 
+  beforeEach(() => {
+    calculateSpy = sinon.spy(Utils, 'calculateNumber');
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+    calculateSpy.restore();
+  });
+
+  it('uses the calculateNumber method of utils', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(spy.calculateNumber.callCount).to.be.equal(1);
-    spy.calculateNumber.restore();
+    expect(calculateSpy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(calculateSpy.callCount).to.be.equal(1);
+    calculateSpy.restore();
+  });
+
+  it('prints the right message to console', () => {
+    sendPaymentRequestToApi(100, 20);
+    sendPaymentRequestToApi(190, 30);
+    expect(consoleSpy.calledWith('The total amount is 120')).to.be.true;
+    expect(consoleSpy.calledWith('The total amount is 220')).to.be.true;
   });
 });
