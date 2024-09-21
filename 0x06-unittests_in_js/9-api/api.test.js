989336d3f9ebe59decf61d426d24ff5f2da776cd
kiplimoboor <kiplimoarapboor@gmail.com>
2024-09-21 18:24:50 +0300

    test

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,24 +1,36 @@
-const { expect } = require('chai');
 const request = require('request');
+const { expect } = require('chai');
 
-describe('test root api route', () => {
-  it('correct response', () => {
-    const res = request('http://127.0.0.1:7865/', (err, res, body) => {
-      expect(res.statusCode === 200).to.be.true;
-      expect(body).to.deep.equal('Welcome to the payment system');
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
+
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
     });
   });
-});
 
-describe('test /cart/ api route', () => {
-  it('correct response', () => {
-    const res = request('http://127.0.0.1:7865/cart/2', (err, res, body) => {
-      expect(res.statusCode == 200).to.be.true;
-      expect(body).to.deep.equal('Payment methods for cart 2');
+  it('GET /cart/:id returns correct response for valid :id', (done) => {
+    request.get(`${API_URL}/cart/47`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Payment methods for cart 47');
+      done();
     });
+  });
 
-    const wrong = request('http://127.0.0.1:7865/cart/hello', (_, res, _) => {
-      expect(res.statusCode == 404).to.be.true;
+  it('GET /cart/:id returns 404 response for negative number values in :id', (done) => {
+    request.get(`${API_URL}/cart/-47`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
+    });
+  });
+
+  it('GET /cart/:id returns 404 response for non-numeric values in :id', (done) => {
+    request.get(`${API_URL}/cart/d200-44a5-9de6`, (_err, res, _body) => {
+      expect(res.statusCode).to.be.equal(404);
+      done();
     });
   });
 });
